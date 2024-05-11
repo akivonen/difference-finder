b5e981c65aa0d32141a6e6c96248ad938c32abc0
Artjom Kivonen <kivonen@gmail.com>
2024-05-11 17:16:05 +0300

    refactoring

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,10 @@
 import fs from 'node:fs';
 import { fileURLToPath } from 'url';
-import { dirname, join } from 'node:path';
+import { dirname, resolve } from 'node:path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const getFilePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
+const getFilePath = (filename) => resolve(__dirname, '..', '__fixtures__', filename);
 
 const readFile = (filepath) => fs.readFileSync(filepath, 'utf-8');
 
